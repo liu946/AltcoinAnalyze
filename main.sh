1f6d738a5f18e4d4e051835f5daa2ffc6ce671a9
liu946 <liuyang570926881@gmail.com>
2014-09-27 16:45:27 +0800

    加入main.sh程序入口
    具备整合json文件功能，另付整合好的json文件

---
+++ main.sh
@@ -0,0 +1,15 @@
+touch data.json
+echo "{" >> data.json
+a=`ls ./stats | wc -w`;
+i=1;
+for dir in `ls ./stats`
+do  
+echo '"'${dir%/}'" : ' >> data.json
+cat 'stats/'$dir"/"`ls stats/$dir`'/stats.json' >> data.json
+if [[ "$a" -ne "$i" ]] 
+then
+echo ','>> data.json
+fi
+let i+=1
+done
+echo "}" >> data.json
