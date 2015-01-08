a4c881d52bb38470268f0b245a14b44f00dcb925
liu946 <liuyang570926881@gmail.com>
2015-01-08 13:08:25 +0800

    axis lable

--- html/radarhtml5.js
+++ html/radarhtml5.js
@@ -1,4 +1,28 @@
-function drawRadarData (canvas,size,array,wei) {
+	function drawRadarxyLable (canvas,size,array,wei) {
+		var c=document.getElementById(canvas);
+		var cxt=c.getContext("2d");
+		cxt.translate(size/2,size/2);
+		cxt.strokeStyle = "rgba(0,0,0,1)"
+		cxt.lineWidth="2";
+		cxt.fillStyle="#000";
+
+		angletotle=0;
+		for (var i = 0 ; i < array.length; i++) {
+			{//写坐标轴lable，移动过去把角度转回来，再写，再恢复
+				cxt.translate(0,size*0.45);
+				cxt.rotate(-angletotle);
+				
+				cxt.fillText(array[i],0,0);
+				cxt.rotate(angletotle);
+				cxt.translate(0,-size*0.45);
+			}
+			
+			cxt.rotate(6.2831853*wei[i]);
+			angletotle+=6.2831853*wei[i];
+		};
+		cxt.translate(-size/2,-size/2);
+	}
+	function drawRadarData (canvas,size,array,wei) {
 		var c=document.getElementById(canvas);
 		var cxt=c.getContext("2d");
 		cxt.fillStyle="#FF8800";
@@ -51,6 +75,8 @@ function drawRadarData (canvas,size,array,wei) {
 		cxt.translate(-size/2,-size/2);
 	}
 	// arr=new Array(0.7,0.5,0.2,0.7,0.6,1);
+	// arrlable=new Array("star","fork","3..","4..","5..","6..");
 	// wei=new Array(0.1,0.1,0.1,0.3,0.2,0.2);
 	// drawRadarxy("myCanvas",500,arr.length,wei);
+	// drawRadarxyLable("myCanvas",500,arrlable,wei);
 	// drawRadarData("myCanvas",500,arr,wei);
